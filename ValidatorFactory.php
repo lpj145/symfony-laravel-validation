baf39b99998678cc96b642fe9a462729fa608699
Marquinho <marcos.adantas@hotmail.com>
2018-06-29 01:14:08 -0300

    Initialized

--- ValidatorFactory.php
+++ ValidatorFactory.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Marquinho
- * Date: 28/06/2018
- * Time: 23:57
- */
-
 namespace MarcosDantas\LaravelValidationBundle;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Translation\FileLoader;
+use Illuminate\Translation as Translation;
+use Illuminate\Validation\Factory;
 
 class ValidatorFactory
 {
+    protected static function loadTranslator(string $locale): Translation\Translator
+    {
+        $fileSystem = new Filesystem();
+        $loader = new Translation\FileLoader(
+            $fileSystem,
+            self::getLocalePath()
+        );
 
+        $loader->addNamespace('lang', self::getLocalePath());
+        $loader->load($locale, 'validation', 'lang');
+
+
+        return new Translation\Translator($loader, $locale);
+    }
+
+    protected static function getLocalePath()
+    {
+        return dirname(__FILE__).'/lang';
+    }
+
+    public static function factoryValidator(string $locale = 'pt-BR')
+    {
+        return new Factory(self::loadTranslator($locale));
+    }
+
+    public function createValidator(string $locale = 'pt-BR')
+    {
+        return self::factoryValidator($locale);
+    }
 }
